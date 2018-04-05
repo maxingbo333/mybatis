0ab6e62e2d6d8d637db846826897c14ffaf840a9
maxingbo <mxb2java@163.com>
2018-04-06 00:51:30 +0800

    ssss

--- src/cn/itcast/mybatis/first/MybatisFirst.java
+++ src/cn/itcast/mybatis/first/MybatisFirst.java
@@ -38,11 +38,6 @@ public class MybatisFirst {
 
 
 
-		// mybatis配置文件
-		String resource = "SqlMapConfig.xml";
-		// 得到配置文件流
-		InputStream inputStream = Resources.getResourceAsStream(resource);
-qqqqqqq
 
 
 	}
